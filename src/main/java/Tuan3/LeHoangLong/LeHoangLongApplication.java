2b43706ac07727de0499a5f6e40600a6f4973d46
Hoang Long <chickbabyhappy>
2024-06-01 14:12:26 +0700

    Initial commit

---
+++ src/main/java/Tuan3/LeHoangLong/LeHoangLongApplication.java
@@ -0,0 +1,13 @@
+package Tuan3.LeHoangLong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeHoangLongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeHoangLongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Tuan3/LeHoangLong/LeHoangLongApplicationTests.java
@@ -0,0 +1,13 @@
+package Tuan3.LeHoangLong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeHoangLongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
